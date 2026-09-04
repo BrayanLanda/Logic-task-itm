1f3c4bc7aba659debf5e8c9b56c9c36f8c8019e8
Brayan Landa <landakkpp@gmail.com>
2026-09-03 21:11:04 -0500

    feat: add validation and fisrt part of problem

---
+++ TriangularMatrix/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var number = ConsoleExtension.GetInt("Ingrese un numero: ");
+}
+while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
