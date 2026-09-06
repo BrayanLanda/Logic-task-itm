79dc075e81f0e669f5b3234a32b56ea7768d7dbb
Brayan Landa <landakkpp@gmail.com>
2026-09-06 18:39:42 -0500

    fix: update target framework to net9.0 and enhance matrix functionality

--- TriangularMatrix/Program.cs
+++ TriangularMatrix/Program.cs
@@ -1,11 +1,47 @@
-﻿
-using Shared;
+﻿using Shared;
 
 var answer = string.Empty;
 var options = new List<string> { "s", "n" };
 
 do
 {
-    var number = ConsoleExtension.GetInt("Ingrese un numero: ");
-}
-while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+    var n = ConsoleExtension.GetInt("Ingrese orden de la matriz: ");
+
+    var matriz = new int[n, n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matriz[i, j] = i + j;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{matriz[i, j],-4}");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write($"{matriz[i, j],-4}");
+        }
+        Console.WriteLine();
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
